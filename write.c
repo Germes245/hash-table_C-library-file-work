52fd9ad0937675b23ca6f39caab543a8a6840aea
shya <dan119@bk.ru>
2026-02-25 15:55:52 +0300

    вынесена записаь в отдельную функцию

--- write.c
+++ write.c
@@ -6,19 +6,18 @@
 
 //#define size_of_block 70
 
-int main() {
-    char *text[] = {"om namah shivaya namah shivaya hara hara bole namah shivaya", "om mani padme hum"};
-    uint32_t size_of_block = 0, number_of_strings_in_text = sizeof(text)/sizeof(char*);
+void write_strings_into_file(char* name_of_file, char *text[], uint32_t number_of_strings_in_text){
+    uint32_t size_of_block = 0;
     for(uint32_t i = 0; i < number_of_strings_in_text; i++){
         uint32_t len = strlen(text[i]);
         printf("len %d\n", len);
         if(len > size_of_block) size_of_block = len + 1;
     }
     printf("size_of_block %d\n", size_of_block);
-    FILE *file = fopen("test.bin", "wb");
+    FILE *file = fopen(name_of_file, "wb");
     if (file == NULL) {
         perror("Ошибка открытия");
-        return 1;
+        exit(1);
     }
     {
         uint8_t size_for_array = sizeof(uint32_t)*2;
@@ -29,11 +28,9 @@ int main() {
         //write(1, array, size_for_array);
     }
     char block[size_of_block];
-    for(uint32_t i = 0; i < size_of_block; i++){
-        block[i] = 0;
-    }
+    for(uint32_t i = 0; i < size_of_block; i++) block[i] = 0;
     uint8_t current_size_of_strings = 0;
-    for(size_t i = 0; i < sizeof(text)/sizeof(void*); i++){
+    for(size_t i = 0; i < number_of_strings_in_text; i++){
         size_t size = strlen(text[i])+1;
         printf("shya\n");
         if(size <= size_of_block - current_size_of_strings){
@@ -49,9 +46,12 @@ int main() {
         }
         current_size_of_strings += size;
     }
-    //write(1, block, size_of_block);
     fwrite(block, sizeof(char), size_of_block, file);
-    //printf("%d", current_size_of_strings);
     fclose(file);
+}
+
+int main() {
+    char *text[] = {"om namah shivaya namah shivaya hara hara bole namah shivaya", "om mani padme hum"};
+    write_strings_into_file("test.bin", text, sizeof(text)/sizeof(char*));
     return 0;
 }
