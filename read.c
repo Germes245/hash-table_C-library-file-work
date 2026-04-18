f7bf37a9827079da972a8811cc8d09a341e0b5e6
shya <dan119@bk.ru>
2026-04-18 19:14:06 +0300

    добавление функции написаной нейросетью

--- read.c
+++ read.c
@@ -1,157 +1,127 @@
-#include <stddef.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <string.h>
 
-typedef struct{
-    char **text;
-    size_t length;
-} char_pointer_array;
+typedef struct {
+    char **strings;  // массив указателей на строки
+    size_t count;    // количество строк
+} StringArray;
 
-#define counto(increment, up_value) for(size_t increment = 0; increment < up_value; increment++)
-
-size_t get_score_of_null_symbols(char buffer[], size_t length){
-    size_t score_of_null_symbols = 0;
-    for(size_t i = 0; i < length; i++){
-        if (buffer[i] == 0) {
-            score_of_null_symbols++;
-        }
+/**
+ * Читает файл, в котором строки разделены символом '\0'.
+ * Возвращает структуру StringArray. В случае ошибки возвращает {NULL, 0}.
+ * Память, выделенную под строки и массив указателей, должен освободить caller.
+ */
+StringArray read_null_separated_file(const char *filename) {
+    StringArray result = {NULL, 0};
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        return result;
     }
-    return score_of_null_symbols;
-}
 
-void get_indexes(size_t indexes[], char buffer[], size_t length){
-    for (size_t i = 0, j = 0; i < length; i++) {
-        if (buffer[i] == 0) {
-            // нахождение индексов NULL символов
-            indexes[j] = i;
-            j++;
-        }
+    // Определяем размер файла
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    if (file_size < 0) {
+        fclose(file);
+        return result;
     }
-}
+    rewind(file);
 
-char_pointer_array read_buffer(char buffer[], size_t length){
-    // 1
-    char_pointer_array strings;
-    size_t score_of_null_symbols = get_score_of_null_symbols(buffer, length); // количество NULL символов
-
-    size_t array_of_indexes[score_of_null_symbols];
-    get_indexes(array_of_indexes, buffer, length);
-
-    // 2
-    if(buffer[length-1]){
-        strings.length = score_of_null_symbols + 1;
-        strings.text = malloc((strings.length + 1) * sizeof(char*));
-        strings.text[strings.length] = 0;
-
-        // копирование первой строки
-        size_t length_of_array = array_of_indexes[0]+1;
-        strings.text[0] = malloc(length_of_array);
-        memcpy(strings.text[0], buffer, length_of_array);
-
-        size_t i = 1;
-        if(strings.length > 2){
-            for(; i < strings.length - 1; i++){
-                length_of_array = array_of_indexes[i] - array_of_indexes[i-1];
-                strings.text[i] = malloc(length_of_array);
-                memcpy(strings.text[i], buffer + array_of_indexes[i-1] + 1, length_of_array);
-            }
-        }
-
-        // копирование последней строки
-        length_of_array = (length-1) - (array_of_indexes[score_of_null_symbols-1]+1);
-        strings.text[i] = malloc(length_of_array);
-        memcpy(strings.text[i], buffer + array_of_indexes[score_of_null_symbols-1]+1, length_of_array);
-        strings.text[i][length_of_array]=0;
-
-        strings.length++;
+    // Читаем весь файл в буфер
+    char *buffer = (char*)malloc(file_size + 1);
+    if (!buffer) {
+        fclose(file);
+        return result;
     }
-    else {
-        // "первый случай"
-        strings.length = score_of_null_symbols;
-        strings.text = malloc(strings.length * sizeof(char*));
-
-        // индексы границ у строк, которые будут скопированы в массив указателей
-        size_t length = array_of_indexes[0]+1;
-        strings.text[0] = malloc(length);
-        memcpy(strings.text[0], buffer, length);
-        counto(i, strings.length-1){
-            length = array_of_indexes[i+1] - array_of_indexes[i]+1;
-            strings.text[i+1] = malloc(length);
-            memcpy(strings.text[i+1], buffer + array_of_indexes[i]+1, length);
-        }
+    size_t bytes_read = fread(buffer, 1, file_size, file);
+    if (bytes_read != (size_t)file_size) {
+        free(buffer);
+        fclose(file);
+        return result;
     }
-    return strings;
-}
-
-char_pointer_array read_strings_from_file(char* name_of_file){
-    // чтение файла в буффер
-    FILE *file = fopen(name_of_file, "rb");
-    if (file == NULL) {
-        perror("Ошибка открытия");
-        exit(1);
-    }
-    char buffer[0x31];
-    size_t length = fread(buffer, 1, sizeof(buffer), file);
-    char_pointer_array array_of_strings = read_buffer(buffer, length);
-    counto(i, array_of_strings.length){
-        printf("%s\n", array_of_strings.text[i]);
-    }
-    putchar('\n');
-    length = fread(buffer, 1, sizeof(buffer), file);
-    putchar(buffer[0]);
-    putchar('\n');
-    char_pointer_array array_of_strings2 = read_buffer(buffer, length);
-    if(array_of_strings.text[array_of_strings.length-1]){
-        size_t index = array_of_strings.length;
-        array_of_strings.length += array_of_strings2.length;
-        array_of_strings.text = realloc(array_of_strings.text, array_of_strings.length * sizeof(char*));
-        counto(i, array_of_strings.length){
-            printf("%d\n", array_of_strings.text[i]);
-        }
-        putchar('\n');
-        for(size_t j = 0; index < array_of_strings.length; index++){
-            array_of_strings.text[index] = array_of_strings2.text[j];
-            j++;
-        }
-        counto(i, array_of_strings.length){
-            printf("%s\n", array_of_strings.text[i]);
-        }
-    }
-    else {
-        printf("strings:\n");
-        counto(i, array_of_strings2.length){
-            printf("%s\n", array_of_strings2.text[i]);
-        }
-        putchar('\n');
-        array_of_strings.text[array_of_strings.length-2] = realloc(array_of_strings.text[array_of_strings.length-2], strlen(array_of_strings.text[array_of_strings.length-2]) + strlen(array_of_strings2.text[0]) + 1);
-        strcat(array_of_strings.text[array_of_strings.length-2], array_of_strings2.text[0]);
-
-        counto(i, array_of_strings.length){
-            printf("%s\n", array_of_strings.text[i]);
-        }
-        
-        /*size_t index = array_of_strings.length-1;
-        printf("длина первого: %d, длина второго: %d\n", array_of_strings.length, array_of_strings2.length);
-        array_of_strings.length += array_of_strings2.length - 1;
-        array_of_strings.text = realloc(array_of_strings.text, array_of_strings.length * sizeof(char*));
-        for(size_t j = 0; index < array_of_strings.length; index++){
-            array_of_strings.text[index] = array_of_strings2.text[j];
-            j++;
-        }
-        printf("новая длина: %d\n", array_of_strings.length);
-        counto(i, array_of_strings.length){
-            printf("%s\n", array_of_strings.text[i]);
-        }*/
-    }
+    buffer[file_size] = '\0';   // для безопасности
     fclose(file);
-    exit(1);
+
+    // Подсчёт количества строк
+    size_t count = 0;
+    for (long i = 0; i < file_size; ++i) {
+        if (buffer[i] == '\0') {
+            ++count;
+        }
+    }
+    if (file_size > 0 && buffer[file_size - 1] != '\0') {
+        ++count;   // последняя строка не завершена нулём
+    }
+
+    // Выделяем массив указателей на строки
+    char **strings = NULL;
+    if (count > 0) {
+        strings = (char**)malloc(sizeof(char*) * count);
+        if (!strings) {
+            free(buffer);
+            return result;
+        }
+    }
+
+    // Разбираем буфер на строки
+    size_t str_idx = 0;
+    size_t start = 0;
+    for (long i = 0; i < file_size; ++i) {
+        if (buffer[i] == '\0') {
+            size_t len = i - start;
+            char *str = (char*)malloc(len + 1);
+            if (!str) {
+                // Ошибка: освобождаем уже выделенное
+                for (size_t j = 0; j < str_idx; ++j) {
+                    free(strings[j]);
+                }
+                free(strings);
+                free(buffer);
+                return result;
+            }
+            memcpy(str, buffer + start, len);
+            str[len] = '\0';
+            strings[str_idx++] = str;
+            start = i + 1;
+        }
+    }
+    // Остаток после последнего нуля (если файл не оканчивается нулём)
+    if (start < (size_t)file_size) {
+        size_t len = file_size - start;
+        char *str = (char*)malloc(len + 1);
+        if (!str) {
+            for (size_t j = 0; j < str_idx; ++j) {
+                free(strings[j]);
+            }
+            free(strings);
+            free(buffer);
+            return result;
+        }
+        memcpy(str, buffer + start, len);
+        str[len] = '\0';
+        strings[str_idx++] = str;
+    }
+
+    free(buffer);
+    result.strings = strings;
+    result.count = count;   // count должно быть равно str_idx
+    return result;
 }
 
-int main(){
-    char_pointer_array text = read_strings_from_file("test.bin");
+int main() {
+    StringArray arr = read_null_separated_file("test.bin");
+    if (arr.strings == NULL) {
+        printf("Ошибка чтения файла или файл пуст.\n");
+        return 1;
+    }
+
+    printf("Найдено строк: %zu\n", arr.count);
+    for (size_t i = 0; i < arr.count; ++i) {
+        printf("[%zu]: \"%s\"\n", i, arr.strings[i]);
+        free(arr.strings[i]);   // освобождаем каждую строку
+    }
+    free(arr.strings);          // освобождаем массив указателей
+
     return 0;
 }
